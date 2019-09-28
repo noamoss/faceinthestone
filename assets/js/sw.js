5ecc5dd83b3533794bd90caa658c1d1b266a7731
noamoss <noam@thepitz.io>
2019-09-28 22:24:11 +0300

    fix server worker, add fetch

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -4,23 +4,41 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cachename)
             .then(cache => cache.addAll([
-                'images/project_logo.png',
-                'images/view.jpg',
-                'favicon.ico',
-                'assets/css/main.css',
+                '/images/project_logo.png',
+                '/images/view.jpg',
+                '/favicon.ico',
+                '/assets/css/main.css',
             ]))
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+
+self.addEventListener('fetch', function(event) {                   
+  event.respondWith(
+    caches.match(event.request)                                    
+      .then(function(response) {
+        if (response) {                                            
+          return response;
+        }
+
+        var requestToCache = event.request.clone();                
+
+        return fetch(requestToCache).then(                         
+          function(response) {
+            if(!response || response.status !== 200) {             
+              return response;
             }
-        )
+
+            var responseToCache = response.clone();                
+
+            caches.open(cacheName)                                 
+              .then(function(cache) {
+                cache.put(requestToCache, responseToCache);        
+              });
+
+            return response;
+          }
+        );
+      })
     );
 });
