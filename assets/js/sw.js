ac42c1e928b965f8cce22adbd6acf8d43203391b
noamoss <noam@thepitz.io>
2019-09-28 20:45:43 +0300

    initial pwa

---
+++ assets/js/sw.js
@@ -0,0 +1,26 @@
+var cachename = 'view';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cachename)
+            .then(cache => cache.addAll([
+                '/images/project_logo.png',
+                '/images/view.jpg',
+                '/favicon.ico',
+                '/assets/css/main.css',
+            ]))
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
